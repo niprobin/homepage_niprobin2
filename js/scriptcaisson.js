57202ac5d99f4c7f8ff2a557ad2706b27db9252b
niprobin <niprobin@gmail.com>
2022-12-09 20:48:58 +0100

    try to get API data in HTML

--- js/scriptcaisson.js
+++ js/scriptcaisson.js
@@ -1,3 +1,22 @@
-$.get("https://admin.lecaisson.live/api/nowplaying", function (status) {
-    alert("Data: " + status);
+var nowPlayingTimeout;
+var nowPlaying;
+
+function loadNowPlaying() {
+    $.ajax({
+        cache: false,
+        dataType: "json",
+        url: 'http://admin-radio.niprobin.com/api/nowplaying/gone',
+        success: function(np) {
+            // Do something with the Now Playing data.
+            nowPlaying = np;
+            $('#nowplaying-song').text(np.now_playing.song.text);
+            nowPlayingTimeout = setTimeout(loadNowPlaying, 15000);
+        }
+    }).fail(function() {
+        nowPlayingTimeout = setTimeout(loadNowPlaying, 30000);
+    });
+}
+
+$(function() {
+    loadNowPlaying();
 });
