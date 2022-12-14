7cb19881a48b0d19e354c36d71af569c555f5f05
niprobin <niprobin@gmail.com>
2022-12-14 18:14:53 +0100

    Update scriptcaisson.js

--- js/scriptcaisson.js
+++ js/scriptcaisson.js
@@ -5,7 +5,7 @@ function loadNowPlaying() {
     $.ajax({
         cache: false,
         dataType: "json",
-        url: 'https://admin-radio.niprobin.com/api/nowplaying/gone',
+        url: 'https://139.177.182.196/api/nowplaying/gone',
         success: function(np) {
             // Do something with the Now Playing data.
             nowPlaying = np;
